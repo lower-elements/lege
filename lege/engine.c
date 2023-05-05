255d31dee874311a2e08e3c7d36bd29eb6d5b16b
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-06 00:20:10 +0100

    Feat: Init / term Lua, and allow loading the main chunk
    This is demonstrated in examples/hello_world.c.

--- lege/engine.c
+++ lege/engine.c
@@ -1,3 +1,6 @@
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -7,18 +10,40 @@
 struct lege_engine {
   const char *app_name;
   const char *org_name;
+  lua_State *L;
 };
 
 lege_engine_t lege_engine_new(void) {
+  // Allocate the engine object
   lege_engine_t engine = lege_xnew(struct lege_engine);
-  if (LIKELY(engine)) {
-    engine->app_name = "LEGE Application";
-    engine->org_name = "lower-elements";
+  if (UNLIKELY(!engine)) {
+    return NULL;
   }
+
+  // Set default options
+  engine->app_name = "LEGE Application";
+  engine->org_name = "lower-elements";
+
+  // Initialize the Lua state
+  engine->L = luaL_newstate();
+  if (UNLIKELY(!engine->L)) {
+    goto err;
+  }
+  luaL_openlibs(engine->L);
+
   return engine;
+
+err:
+  lege_free(engine);
+  return NULL;
 }
 
-void lege_engine_free(lege_engine_t engine) { lege_free(engine); }
+void lege_engine_free(lege_engine_t engine) {
+  if (LIKELY(engine)) {
+    lua_close(engine->L);
+    lege_free(engine);
+  }
+}
 
 bool lege_engine_set_string(lege_engine_t engine, lege_option_t opt,
                             const char *str) {
@@ -34,6 +59,12 @@ bool lege_engine_set_string(lege_engine_t engine, lege_option_t opt,
   }
 }
 
+bool lege_engine_load(lege_engine_t engine, const char *buff, size_t sz,
+                      const char *name) {
+  return luaL_loadbuffer(engine->L, buff, sz, name) != 0;
+}
+
 void lege_engine_run(lege_engine_t engine) {
   printf("Starting %s.%s\n", engine->org_name, engine->app_name);
+  lua_call(engine->L, 0, 0);
 }
