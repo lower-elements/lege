55338f7a108a2891df5560b69736ee242c37c2fb
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-06 01:11:16 +0100

    Feat: Allow Lua packages to be preloaded
    lege_engine_preload() parses the Lua code into a function, and attaches
    it to a C closure, which is then set as package.preload[name] where name
    is the chunk name atm. The closure checks that the name passed to it
    matches the name of the preloaded chunk, then runs the chunk, hense
    loading the package.

---
+++ examples/preload.c
@@ -0,0 +1,30 @@
+#include <lege.h>
+#include <stdio.h>
+
+static const char module[] = "\
+                              print('Test module loaded')\
+                              return {\
+                                  x = 5,\
+                                      y = 10,\
+                              }\
+                              ";
+
+static const char entrypoint[] = "\
+                                   local mod = require('test_module')\
+                                   print(mod.x + mod.y)\
+                                   ";
+
+int main(void) {
+  lege_engine_t engine = lege_engine_new();
+  if (!engine) {
+    perror("Failed to create engine");
+    return 1;
+  }
+  lege_engine_set_string(engine, LEGE_OPTION_APP_NAME, "Hello World");
+  lege_engine_set_string(engine, LEGE_OPTION_ORG_NAME, "lower-elements");
+  lege_engine_preload_literal(engine, module, "test_module");
+  lege_engine_load_literal(engine, entrypoint, "init.lua");
+  lege_engine_run(engine);
+  lege_engine_free(engine);
+  return 0;
+}
--- lege/engine.c
+++ lege/engine.c
@@ -3,6 +3,7 @@
 #include <lualib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "lege.h"
 #include "util.h"
@@ -62,6 +63,41 @@ bool lege_engine_set_string(lege_engine_t engine, lege_option_t opt,
 bool lege_engine_load(lege_engine_t engine, const char *buff, size_t sz,
                       const char *name) {
   return luaL_loadbuffer(engine->L, buff, sz, name) != 0;
+}
+
+static int load_preloaded(lua_State *L) {
+  // Check first argument (the package to load) is a string
+  const char *pkg = luaL_checkstring(L, 1);
+  // Check it's the same as the name of the package this closure loads
+  lua_pushvalue(L, lua_upvalueindex(1));
+  if (!lua_rawequal(L, 1, -1)) {
+    // Not the same package
+    return luaL_error(L, "Failed to load package %s", pkg);
+  }
+
+  // Retrieve and call the function for this particular package
+  lua_pushvalue(L, lua_upvalueindex(2));
+  lua_call(L, 0, 1);
+  return 1;
+}
+
+void lege_engine_preload(lege_engine_t engine, const char *buff, size_t sz,
+                         const char *name) {
+  // Get the package.preload table
+  lua_getglobal(engine->L, "package");
+  lua_pushliteral(engine->L, "preload");
+  lua_rawget(engine->L, -2);
+  // Push the key we want to set on package.preload
+  lua_pushstring(engine->L, name);
+  // Push it again, as it's the first upvalue for our preload closure
+  lua_pushstring(engine->L, name);
+  // Load code from the buffer, storing it as a function at the stack top
+  luaL_loadbuffer(engine->L, buff, sz, name);
+  // Construct a closure with the two top stack items
+  // This closure runs the preloaded code, hense loading the package
+  lua_pushcclosure(engine->L, load_preloaded, 2);
+  // package.preload[name] = closure
+  lua_rawset(engine->L, -3);
 }
 
 void lege_engine_run(lege_engine_t engine) {
