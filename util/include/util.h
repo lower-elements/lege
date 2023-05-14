c85b2270aa5e0b3b9481ab2ec49b9e40af0688f3
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-14 18:48:41 +0100

    Feat[util]: arraysize() macro

--- util/include/util.h
+++ util/include/util.h
@@ -10,6 +10,8 @@
 #define LEGE_ALLOC_SIZE(...)
 #endif
 
+#define arraysize(a) (sizeof(a) / sizeof((a)[0]))
+
 // Memory allocation
 // Functions / macros with an `x` return NULL on failure, the others abort().
 #define lege_xmalloc malloc
