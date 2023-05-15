31c8f9f86a53c4ce398cab4b42bb933e3ac1cd18
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-15 02:37:55 +0100

    Feat[examples]: Add use_strict example

---
+++ examples/using_strict.lua
@@ -0,0 +1,9 @@
+require "lege/strict"
+
+-- With strict, accessing global variables from anywhere that aren't first declared is an error
+-- print("The value of x is " .. tostring(x)) -- Error: variable 'x' is not declared
+
+-- You declare global variables anywhere in a main chunk (I.E. not in a function)
+-- They can be set to anything, even nil!
+x = nil
+print("The value of x is " .. tostring(x)) --> The value of x is nil
