9b0058c57a36188e9705666899a19f161712f33e
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-19 17:37:12 +0100

    Doc[engine]: Improve lege.log docs with links to the SDL wiki
    Added a custom @see tag handler to link any references starting with
    "sdl:" to the relevent page on the SDL2 wiki.

--- log.c
+++ log.c
@@ -39,6 +39,7 @@ static const char *const LOG_PRIORITIES[SDL_NUM_LOG_PRIORITIES] = {
  * @function get_priority
  * @treturn string The current SDL log priority
  * @see set_priority
+ * @see sdl:SDL_LogGetPriority
  */
 static int log_get_priority(lua_State *L) {
   int idx = SDL_LogGetPriority(SDL_LOG_CATEGORY_APPLICATION) -
@@ -53,6 +54,7 @@ static int log_get_priority(lua_State *L) {
  * @tparam string priority The new SDL log priority
  * @raise if priority is not a valid log message priority
  * @see get_priority
+ * @see sdl:SDL_LogSetPriority
  */
 static int log_set_priority(lua_State *L) {
   SDL_LogPriority pri =
@@ -67,6 +69,7 @@ static int log_set_priority(lua_State *L) {
  * @tparam string priority The priority of the log message
  * @tparam string message The message to log
  * @raise if priority is not a valid log message priority
+ * @see sdl:SDL_LogMessage
  */
 static int log_message(lua_State *L) {
   SDL_LogPriority pri =
@@ -80,6 +83,7 @@ static int log_message(lua_State *L) {
  * Log a message at the verbose priority.
  * @function verbose
  * @tparam string message The message to log
+ * @see sdl:SDL_LogVerbose
  */
 static int log_verbose(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
@@ -91,6 +95,7 @@ static int log_verbose(lua_State *L) {
  * Log a message at the debug priority.
  * @function debug
  * @tparam string message The message to log
+ * @see sdl:SDL_LogDebug
  */
 static int log_debug(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
@@ -102,6 +107,7 @@ static int log_debug(lua_State *L) {
  * Log a message at the info priority.
  * @function info
  * @tparam string message The message to log
+ * @see sdl:SDL_LogInfo
  */
 static int log_info(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
@@ -113,6 +119,7 @@ static int log_info(lua_State *L) {
  * Log a message at the warn priority.
  * @function warn
  * @tparam string message The message to log
+ * @see sdl:SDL_LogWarn
  */
 static int log_warn(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
@@ -124,6 +131,7 @@ static int log_warn(lua_State *L) {
  * Log a message at the error priority.
  * @function error
  * @tparam string message The message to log
+ * @see sdl:SDL_LogError
  */
 static int log_error(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
@@ -135,6 +143,7 @@ static int log_error(lua_State *L) {
  * Log a message at the critical priority.
  * @function critical
  * @tparam string message The message to log
+ * @see sdl:SDL_LogCritical
  */
 static int log_critical(lua_State *L) {
   const char *str = luaL_checkstring(L, 1);
