f3524e5017e99913f767bb2afa9ce31a862c58c6
Michael Connor Buchan <mikey@blindcomputing.org>
2023-05-30 15:29:43 +0100

    Feat[util]: Add INITIALIZER macro for cross-compiler constructor functions

--- util/include/util.h
+++ util/include/util.h
@@ -1,6 +1,7 @@
 #ifndef LIBLEGE_UTIL_H
 #define LIBLEGE_UTIL_H
 
+#include <SDL_stdinc.h>
 #include <hedley.h>
 #include <stdlib.h>
 
@@ -11,12 +12,40 @@
 #endif
 
 #if HEDLEY_HAS_BUILTIN(__builtin_types_compatible_p)
-#define assert_types_compatible(t1, t2)                                        \
-  SDL_COMPILE_TIME_ASSERT(__builtin_types_compatible_p((t1), (t2)))
+#define assert_types_compatible(name, t1, t2)                                  \
+  SDL_COMPILE_TIME_ASSERT(name, __builtin_types_compatible_p(t1, t2))
 #else
-#define assert_types_compatible(t1, t2)                                        \
-  SDL_COMPILE_TIME_ASSERT(sizeof(t1) == sizeof(t2) &&                          \
-                          _Alignof(t1) == _Alignof(t2))
+#define assert_types_compatible(name, t1, t2)                                  \
+  SDL_COMPILE_TIME_ASSERT(name, sizeof(t1) == sizeof(t2) &&                    \
+                                    _Alignof(t1) == _Alignof(t2))
+#endif
+
+// Cross-compiler constructor functions
+// https://stackoverflow.com/questions/1113409/attribute-constructor-equivalent-in-vc
+// 2010-2016 Joe Lowe. Released into the public domain.
+#ifdef __cplusplus
+#define INITIALIZER(f)                                                         \
+  static void f(void);                                                         \
+  struct f##_t_ {                                                              \
+    f##_t_(void) { f(); }                                                      \
+  };                                                                           \
+  static f##_t_ f##_;                                                          \
+  static void f(void)
+#elif defined(_MSC_VER)
+#pragma section(".CRT$XCU", read)
+#define INITIALIZER2_(f, p)                                                    \
+  static void f(void);                                                         \
+  __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f;                     \
+  __pragma(comment(linker, "/include:" p #f "_")) static void f(void)
+#ifdef _WIN64
+#define INITIALIZER(f) INITIALIZER2_(f, "")
+#else
+#define INITIALIZER(f) INITIALIZER2_(f, "_")
+#endif
+#else
+#define INITIALIZER(f)                                                         \
+  static void f(void) __attribute__((constructor));                            \
+  static void f(void)
 #endif
 
 #define arraysize(a) (sizeof(a) / sizeof((a)[0]))
