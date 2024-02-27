febba7937f3f5bfbb93da3700b775f57b7d27a82
Michael Connor Buchan <mikey@blindcomputing.org>
2024-02-27 15:07:57 +0000

    feat: Byte-compile and embed builtin Lua modules
    FOr now this is only c_libs, but in future more builtin modules will be
    written in Lua.

--- lib/builtins.cpp
+++ lib/builtins.cpp
@@ -1,8 +1,13 @@
 #include "builtins.hpp"
 
+// Byte-compiled Lua modules
+#include "modules/lmod_c_libs.h"
+
 namespace lege::modules {
 
 void register_builtins(EngineImpl &e) {
+  e.load((const char *)luaJIT_BC_c_libs, luaJIT_BC_c_libs_SIZE, "b",
+         "lege.c_libs");
   e.load(luaopen_lege_log, "lege.log");
   e.load(luaopen_lege_enum, "lege.enum");
   e.load(luaopen_lege_readonly, "lege.readonly");
