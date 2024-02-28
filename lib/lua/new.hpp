1dba7430f48930ab891530c1e6029a242a59e8a2
Michael Connor Buchan <mikey@blindcomputing.org>
2024-02-28 10:49:28 +0000

    feat: Make sure Lua userdata types are aligned correctly

--- lib/lua/new.hpp
+++ lib/lua/new.hpp
@@ -1,30 +1,58 @@
 #ifndef LIBLEGE_LUA_NEW_HPP
 #define LIBLEGE_LUA_NEW_HPP
 
+#include <cstddef>
 #include <utility>
 
 #include <lua.hpp>
 
 #include "lua/table_view.hpp"
+#include "util.hpp"
 
 namespace lege::lua {
 
+// We assume we'll get something at least 16 byte aligned since LuaJIT seems to
+// align to 16 bytes
+// Todo: Determine this at configure-time?
+inline constexpr std::size_t MIN_USERDATA_ALIGNMENT = 16;
+
 template <class T, class... Args>
 T *new_userdata(lua_State *L, Args &&...args) {
-  void *ptr = lua_newuserdata(L, sizeof(T));
+  std::size_t alloc_size = sizeof(T);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    alloc_size += MIN_USERDATA_ALIGNMENT;
+  }
+  void *ptr = lua_newuserdata(L, alloc_size);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    ptr = align_up(ptr, alignof(T));
+  }
   return new (ptr) T(std::forward(args...));
 }
 
 template <class T, class... Args>
-T *new_userdata(lua_State *L, const char *metatable, Args &&...args) {
-  void *ptr = lua_newuserdata(L, sizeof(T));
+T *new_userdata_mt(lua_State *L, const char *metatable, Args &&...args) {
+  std::size_t alloc_size = sizeof(T);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    alloc_size += MIN_USERDATA_ALIGNMENT;
+  }
+  void *ptr = lua_newuserdata(L, alloc_size);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    ptr = align_up(ptr, alignof(T));
+  }
   luaL_setmetatable(L, metatable);
   return new (ptr) T(std::forward(args...));
 }
 
 template <class T, class... Args>
-T *new_userdata(lua_State *L, int metatable_index, Args &&...args) {
-  void *ptr = lua_newuserdata(L, sizeof(T));
+T *new_userdata_mt(lua_State *L, int metatable_index, Args &&...args) {
+  std::size_t alloc_size = sizeof(T);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    alloc_size += MIN_USERDATA_ALIGNMENT;
+  }
+  void *ptr = lua_newuserdata(L, alloc_size);
+  if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
+    ptr = align_up(ptr, alignof(T));
+  }
   lua_pushvalue(L, metatable_index);
   lua_setmetatable(L, -2);
   return new (ptr) T(std::forward(args...));
