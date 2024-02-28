042df3262633f267a4b23689c99296fc5dbd9e2b
Michael Connor Buchan <mikey@blindcomputing.org>
2024-02-28 13:10:50 +0000

    feat: Functions for automatically creating metatables for C++ types
    Right now only __gc and __name are handled automatically, but other
    metamethods may be automatically generated in future.

--- lib/lua/new.hpp
+++ lib/lua/new.hpp
@@ -2,6 +2,7 @@
 #define LIBLEGE_LUA_NEW_HPP
 
 #include <cstddef>
+#include <type_traits>
 #include <utility>
 
 #include <lua.hpp>
@@ -16,6 +17,35 @@ namespace lege::lua {
 // Todo: Determine this at configure-time?
 inline constexpr std::size_t MIN_USERDATA_ALIGNMENT = 16;
 
+template <class T> void make_metatable(lua_State *L) {
+  // Each C++ type has its own distinct metatable. We ensure uniqueness by
+  // getting a unique hash for the specific type we're creating a metatable for
+  std::string mt_name(fmt::format("mt.{}", lege::type_id<T>()));
+  if (!luaL_newmetatable(L, mt_name.c_str())) {
+    return; // mt already exists
+  }
+
+  // Construct the new metatable
+  // Set mt.__name to a human readable type signature for the C++ type
+  lua_pushliteral(L, "__name");
+  auto name = lege::demangled_name<T>();
+  lua_pushstring(L, name.get());
+  lua_rawset(L, -3);
+
+  if constexpr (std::is_destructible_v<T>) {
+    // Call the destructor upon garbage collection
+    lua_pushliteral(L, "__gc");
+    lua_pushcfunction(L, [](lua_State *L) {
+      T *ptr = static_cast<T *>(lua_touserdata(L, 1));
+      ptr->~T();
+      return 0;
+    });
+    lua_rawset(L, -3);
+  }
+  // Todo: Add more metatable fields automatically (E.G. __add for types that
+  // implement operator+(), etc)
+}
+
 template <class T, class... Args>
 T *new_userdata(lua_State *L, Args &&...args) {
   std::size_t alloc_size = sizeof(T);
@@ -26,7 +56,12 @@ T *new_userdata(lua_State *L, Args &&...args) {
   if constexpr (alignof(T) > MIN_USERDATA_ALIGNMENT) {
     ptr = align_up(ptr, alignof(T));
   }
-  return new (ptr) T(std::forward(args...));
+
+  // Get and / or create metatable
+  make_metatable<T>(L);
+  lua_setmetatable(L, -2);
+
+  return new (ptr) T(std::forward<Args...>(args...));
 }
 
 template <class T, class... Args>
@@ -40,7 +75,7 @@ T *new_userdata_mt(lua_State *L, const char *metatable, Args &&...args) {
     ptr = align_up(ptr, alignof(T));
   }
   luaL_setmetatable(L, metatable);
-  return new (ptr) T(std::forward(args...));
+  return new (ptr) T(std::forward<Args...>(args...));
 }
 
 template <class T, class... Args>
@@ -55,7 +90,7 @@ T *new_userdata_mt(lua_State *L, int metatable_index, Args &&...args) {
   }
   lua_pushvalue(L, metatable_index);
   lua_setmetatable(L, -2);
-  return new (ptr) T(std::forward(args...));
+  return new (ptr) T(std::forward<Args...>(args...));
 }
 
 static inline TableView new_table(lua_State *L, int narr = 0, int nrec = 0) {
--- lib/util.hpp
+++ lib/util.hpp
@@ -2,6 +2,15 @@
 #define LIBLEGE_UTIL_HPP
 
 #include <cstdint>
+#include <cstdlib>
+#include <cxxabi.h>
+#include <exception>
+#include <memory>
+#include <new>
+#include <type_traits>
+#include <typeindex>
+
+#include <fmt/core.h>
 
 namespace lege {
 
@@ -13,6 +22,32 @@ static inline void *align_down(void *ptr, std::uintptr_t alignment) {
 
 static inline void *align_up(void *ptr, std::uintptr_t alignment) {
   return (void *)((std::uintptr_t)align_down(ptr, alignment) + alignment);
+}
+
+template <class T> std::size_t type_id() {
+  return std::type_index(typeid(T)).hash_code();
+}
+
+template <class T>
+std::unique_ptr<char[], void (*)(void *)>
+demangled_name(std::size_t *size = nullptr) {
+  const char *mangled_name = typeid(T).name();
+  int status;
+  char *res = abi::__cxa_demangle(mangled_name, nullptr, size, &status);
+  switch (status) {
+  case 0:
+    return {res, std::free};
+  case -1:
+    throw std::bad_alloc();
+  case -2:
+    throw std::runtime_error(
+        fmt::format("Invalid mangled type name: {}", mangled_name));
+  case -3:
+    throw std::invalid_argument("Invalid argument to __cxa_demangle");
+  default:
+    throw std::runtime_error(
+        fmt::format("Could not demangle type name: {}", mangled_name));
+  }
 }
 
 } // namespace lege
