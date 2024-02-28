d49f0879bdf5d5b71e6b15028e39ee4b10d48ad7
Michael Connor Buchan <mikey@blindcomputing.org>
2024-02-28 10:48:55 +0000

    feat: align_down() and align_up() util functions

--- lib/util.hpp
+++ lib/util.hpp
@@ -1,10 +1,20 @@
 #ifndef LIBLEGE_UTIL_HPP
 #define LIBLEGE_UTIL_HPP
 
+#include <cstdint>
+
 namespace lege {
 
 void setSDLLogPriority();
 
+static inline void *align_down(void *ptr, std::uintptr_t alignment) {
+  return (void *)((std::uintptr_t)ptr & ~(alignment - 1));
 }
+
+static inline void *align_up(void *ptr, std::uintptr_t alignment) {
+  return (void *)((std::uintptr_t)align_down(ptr, alignment) + alignment);
+}
+
+} // namespace lege
 
 #endif
