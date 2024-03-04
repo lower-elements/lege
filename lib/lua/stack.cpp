a24c8ef7dad4ac262b8f4f2bdcccb1b613f7ab9f
Michael Connor Buchan <mikey@blindcomputing.org>
2024-03-04 17:31:37 +0000

    feat: Inline Lua stack helpers that return what they retrieve

--- lib/lua/stack.cpp
+++ lib/lua/stack.cpp
@@ -3,191 +3,27 @@
 
 namespace lege::lua {
 
-void get(lua_State *L, int index, lua_Number &val) {
-  val = lua_tonumber(L, index);
+// These have to be here otherwise we'd have a circular reference to TableView
+TableView &get(lua_State *L, int index, TableView &val) {
+  val = TableView(L, index);
+  return val;
 }
 
-void get(lua_State *L, int index, bool &val) {
-  val = static_cast<bool>(lua_toboolean(L, index));
-}
-
-void get(lua_State *L, int index, std::string_view &val) {
-  std::size_t len;
-  const char *str = lua_tolstring(L, index, &len);
-  val = std::string_view(str, len);
-}
-
-void get(lua_State *L, int index, std::string &val) {
-  std::size_t len;
-  const char *str = lua_tolstring(L, index, &len);
-  val.assign(str, len);
-}
-
-void get(lua_State *L, int index, const char *&val) {
-  val = lua_tostring(L, index);
-}
-
-void get(lua_State *L, int index, lua_CFunction &val) {
-  val = lua_tocfunction(L, index);
-}
-
-void get(lua_State *L, int index, void *&val) {
-  val = lua_touserdata(L, index);
-}
-
-void get(lua_State *L, int index, lua_Integer &val) {
-  val = lua_tointeger(L, index);
-}
-
-void get(lua_State *L, int index, lua_State *&val) {
-  val = lua_tothread(L, index);
-}
-
-void get(lua_State *L, int index, TableView &val) { val = TableView(L, index); }
-
-void arg(lua_State *L, int index, lua_Number &val) {
-  val = luaL_checknumber(L, index);
-}
-
-void arg(lua_State *L, int index, bool &val) {
-  luaL_checktype(L, index, LUA_TBOOLEAN);
-  val = static_cast<bool>(lua_toboolean(L, index));
-}
-
-void arg(lua_State *L, int index, std::string_view &val) {
-  std::size_t len;
-  const char *str = luaL_checklstring(L, index, &len);
-  val = std::string_view(str, len);
-}
-
-void arg(lua_State *L, int index, std::string &val) {
-  std::size_t len;
-  const char *str = luaL_checklstring(L, index, &len);
-  val.assign(str, len);
-}
-
-void arg(lua_State *L, int index, const char *&val) {
-  val = luaL_checkstring(L, index);
-}
-
-void arg(lua_State *L, int index, lua_CFunction &val) {
-  val = lua_tocfunction(L, index);
-  luaL_argcheck(L, val != nullptr, index, "`CFunction` expected");
-}
-
-void arg(lua_State *L, int index, void *&val, const char *mt) {
-  val = luaL_checkudata(L, index, mt);
-}
-
-void arg(lua_State *L, int index, lua_Integer &val) {
-  val = luaL_checkinteger(L, index);
-}
-
-void arg(lua_State *L, int index, lua_State *&val) {
-  luaL_checktype(L, index, LUA_TTHREAD);
-  val = lua_tothread(L, index);
-}
-
-void arg(lua_State *L, int index, TableView &val) {
+TableView &arg(lua_State *L, int index, TableView &val) {
   luaL_checktype(L, index, LUA_TTABLE);
   val = TableView(L, index);
+  return val;
 }
 
-void opt_arg(lua_State *L, int index, lua_Number &val, lua_Number def) {
-  val = luaL_optnumber(L, index, def);
-}
-
-void opt_arg(lua_State *L, int index, bool &val, bool def) {
-  if (lua_isnil(L, index)) {
-    val = def;
-    return;
-  }
-  luaL_checktype(L, index, LUA_TBOOLEAN);
-  val = static_cast<bool>(lua_toboolean(L, index));
-}
-
-void opt_arg(lua_State *L, int index, std::string_view &val,
-             std::string_view def) {
-  std::size_t len = def.size();
-  const char *str = luaL_optlstring(L, index, def.data(), &len);
-  val = std::string_view(str, len);
-}
-
-void opt_arg(lua_State *L, int index, std::string &val, std::string def) {
-  std::size_t len;
-  const char *str = luaL_optlstring(L, index, def.c_str(), &len);
-  if (str == def.c_str()) {
-    val = def;
-  } else {
-    val.assign(str, len);
-  }
-}
-
-void opt_arg(lua_State *L, int index, const char *&val, const char *def) {
-  val = luaL_optstring(L, index, def);
-}
-
-void opt_arg(lua_State *L, int index, lua_CFunction &val, lua_CFunction def) {
-  if (lua_isnil(L, index)) {
-    val = def;
-    return;
-  }
-  val = lua_tocfunction(L, index);
-  luaL_argcheck(L, val != nullptr, index, "`CFunction` expected");
-}
-
-void opt_arg(lua_State *L, int index, void *&val, const char *mt, void *def) {
-  if (lua_isnil(L, index)) {
-    val = def;
-    return;
-  }
-  val = luaL_checkudata(L, index, mt);
-}
-
-void opt_arg(lua_State *L, int index, lua_Integer &val, lua_Integer def) {
-  val = luaL_optinteger(L, index, def);
-}
-
-void opt_arg(lua_State *L, int index, lua_State *&val, lua_State *def) {
-  if (lua_isnil(L, index)) {
-    val = def;
-    return;
-  }
-  luaL_checktype(L, index, LUA_TTHREAD);
-  val = lua_tothread(L, index);
-}
-
-void opt_arg(lua_State *L, int index, std::optional<TableView> &val) {
+std::optional<TableView> &opt_arg(lua_State *L, int index,
+                                  std::optional<TableView> &val) {
   if (lua_isnil(L, index)) {
     val = {};
-    return;
+    return val;
   }
   luaL_checktype(L, index, LUA_TTABLE);
   val = TableView(L, index);
+  return val;
 }
-
-void push(lua_State *L, Nil) { lua_pushnil(L); }
-
-void push(lua_State *L, lua_Number val) { lua_pushnumber(L, val); }
-
-void push(lua_State *L, bool val) { lua_pushboolean(L, val); }
-
-void push(lua_State *L, std::string_view val) {
-  lua_pushlstring(L, val.data(), val.size());
-}
-
-void push(lua_State *L, const std::string &val) {
-  lua_pushlstring(L, val.data(), val.size());
-}
-
-void push(lua_State *L, const char *val) { lua_pushstring(L, val); }
-
-void push(lua_State *L, lua_CFunction val, int nups) {
-  lua_pushcclosure(L, val, nups);
-}
-
-void push(lua_State *L, void *val) { lua_pushlightuserdata(L, val); }
-
-void push(lua_State *L, lua_Integer val) { lua_pushinteger(L, val); }
 
 } // namespace lege::lua
--- lib/lua/stack.hpp
+++ lib/lua/stack.hpp
@@ -17,52 +17,228 @@ static inline int absindex(lua_State *L, int index) {
   return index >= 0 ? index : lua_gettop(L) + index + 1;
 }
 
-void get(lua_State *L, int index, lua_Number &val);
-void get(lua_State *L, int index, bool &val);
-void get(lua_State *L, int index, std::string_view &val);
-void get(lua_State *L, int index, std::string &val);
-void get(lua_State *L, int index, const char *&val);
-void get(lua_State *L, int index, lua_CFunction &val);
-void get(lua_State *L, int index, void *&val);
-void get(lua_State *L, int index, lua_Integer &val);
-void get(lua_State *L, int index, lua_State *&val);
-void get(lua_State *L, int index, TableView &val);
+static inline lua_Number &get(lua_State *L, int index, lua_Number &val) {
+  val = lua_tonumber(L, index);
+  return val;
+}
 
-void arg(lua_State *L, int index, lua_Number &val);
-void arg(lua_State *L, int index, bool &val);
-void arg(lua_State *L, int index, std::string_view &val);
-void arg(lua_State *L, int index, std::string &val);
-void arg(lua_State *L, int index, const char *&val);
-void arg(lua_State *L, int index, lua_CFunction &val);
-void arg(lua_State *L, int index, void *&val, const char *mt);
-void arg(lua_State *L, int index, lua_Integer &val);
-void arg(lua_State *L, int index, lua_State *&val);
-void arg(lua_State *L, int index, TableView &val);
+static inline bool &get(lua_State *L, int index, bool &val) {
+  val = static_cast<bool>(lua_toboolean(L, index));
+  return val;
+}
 
-void opt_arg(lua_State *L, int index, lua_Number &val, lua_Number def = 0.0);
-void opt_arg(lua_State *L, int index, bool &val, bool def = 0);
-void opt_arg(lua_State *L, int index, std::string_view &val,
-             std::string_view def = "");
-void opt_arg(lua_State *L, int index, std::string &val, std::string def = "");
-void opt_arg(lua_State *L, int index, const char *&val, const char *def = "");
-void opt_arg(lua_State *L, int index, lua_CFunction &val,
-             lua_CFunction def = nullptr);
-void opt_arg(lua_State *L, int index, void *&val, const char *mt,
-             void *def = nullptr);
-void opt_arg(lua_State *L, int index, lua_Integer &val, lua_Integer def = 0);
-void opt_arg(lua_State *L, int index, lua_State *&val,
-             lua_State *def = nullptr);
-void opt_arg(lua_State *L, int index, std::optional<TableView> &val);
+static inline std::string_view &get(lua_State *L, int index,
+                                    std::string_view &val) {
+  std::size_t len;
+  const char *str = lua_tolstring(L, index, &len);
+  val = std::string_view(str, len);
+  return val;
+}
 
-void push(lua_State *L, Nil);
-void push(lua_State *L, lua_Number val);
-void push(lua_State *L, bool val);
-void push(lua_State *L, std::string_view val);
-void push(lua_State *L, const std::string &val);
-void push(lua_State *L, const char *val);
-void push(lua_State *L, lua_CFunction val, int nups = 0);
-void push(lua_State *L, void *val);
-void push(lua_State *L, lua_Integer val);
+static inline std::string &get(lua_State *L, int index, std::string &val) {
+  std::size_t len;
+  const char *str = lua_tolstring(L, index, &len);
+  val.assign(str, len);
+  return val;
+}
+
+static inline const char *&get(lua_State *L, int index, const char *&val) {
+  val = lua_tostring(L, index);
+  return val;
+}
+
+static inline lua_CFunction &get(lua_State *L, int index, lua_CFunction &val) {
+  val = lua_tocfunction(L, index);
+  return val;
+}
+
+static inline void *&get(lua_State *L, int index, void *&val) {
+  val = lua_touserdata(L, index);
+  return val;
+}
+
+static inline lua_Integer &get(lua_State *L, int index, lua_Integer &val) {
+  val = lua_tointeger(L, index);
+  return val;
+}
+
+static inline lua_State *&get(lua_State *L, int index, lua_State *&val) {
+  val = lua_tothread(L, index);
+  return val;
+}
+
+TableView &get(lua_State *L, int index, TableView &val);
+
+static inline lua_Number &arg(lua_State *L, int index, lua_Number &val) {
+  val = luaL_checknumber(L, index);
+  return val;
+}
+
+static inline bool &arg(lua_State *L, int index, bool &val) {
+  luaL_checktype(L, index, LUA_TBOOLEAN);
+  val = static_cast<bool>(lua_toboolean(L, index));
+  return val;
+}
+
+static inline std::string_view &arg(lua_State *L, int index,
+                                    std::string_view &val) {
+  std::size_t len;
+  const char *str = luaL_checklstring(L, index, &len);
+  val = std::string_view(str, len);
+  return val;
+}
+
+static inline std::string &arg(lua_State *L, int index, std::string &val) {
+  std::size_t len;
+  const char *str = luaL_checklstring(L, index, &len);
+  val.assign(str, len);
+  return val;
+}
+
+static inline const char *&arg(lua_State *L, int index, const char *&val) {
+  val = luaL_checkstring(L, index);
+  return val;
+}
+
+static inline lua_CFunction &arg(lua_State *L, int index, lua_CFunction &val) {
+  val = lua_tocfunction(L, index);
+  luaL_argcheck(L, val != nullptr, index, "`CFunction` expected");
+  return val;
+}
+
+static inline void *&arg(lua_State *L, int index, void *&val, const char *mt) {
+  val = luaL_checkudata(L, index, mt);
+  return val;
+}
+
+static inline lua_Integer &arg(lua_State *L, int index, lua_Integer &val) {
+  val = luaL_checkinteger(L, index);
+  return val;
+}
+
+static inline lua_State *&arg(lua_State *L, int index, lua_State *&val) {
+  luaL_checktype(L, index, LUA_TTHREAD);
+  val = lua_tothread(L, index);
+  return val;
+}
+
+TableView &arg(lua_State *L, int index, TableView &val);
+
+static inline lua_Number &opt_arg(lua_State *L, int index, lua_Number &val,
+                                  lua_Number def) {
+  val = luaL_optnumber(L, index, def);
+  return val;
+}
+
+static inline bool &opt_arg(lua_State *L, int index, bool &val, bool def) {
+  if (lua_isnil(L, index)) {
+    val = def;
+    return val;
+    ;
+  }
+  luaL_checktype(L, index, LUA_TBOOLEAN);
+  val = static_cast<bool>(lua_toboolean(L, index));
+  return val;
+}
+
+static inline std::string_view &
+opt_arg(lua_State *L, int index, std::string_view &val, std::string_view def) {
+  std::size_t len = def.size();
+  const char *str = luaL_optlstring(L, index, def.data(), &len);
+  val = std::string_view(str, len);
+  return val;
+}
+
+static inline std::string &opt_arg(lua_State *L, int index, std::string &val,
+                                   std::string def) {
+  std::size_t len;
+  const char *str = luaL_optlstring(L, index, def.c_str(), &len);
+  if (str == def.c_str()) {
+    val = def;
+  } else {
+    val.assign(str, len);
+  }
+  return val;
+}
+
+static inline const char *&opt_arg(lua_State *L, int index, const char *&val,
+                                   const char *def) {
+  val = luaL_optstring(L, index, def);
+  return val;
+}
+
+static inline lua_CFunction &opt_arg(lua_State *L, int index,
+                                     lua_CFunction &val, lua_CFunction def) {
+  if (lua_isnil(L, index)) {
+    val = def;
+    return val;
+  }
+  val = lua_tocfunction(L, index);
+  luaL_argcheck(L, val != nullptr, index, "`CFunction` expected");
+  return val;
+}
+
+static inline void *&opt_arg(lua_State *L, int index, void *&val,
+                             const char *mt, void *def) {
+  if (lua_isnil(L, index)) {
+    val = def;
+    return val;
+  }
+  val = luaL_checkudata(L, index, mt);
+  return val;
+}
+
+static inline lua_Integer &opt_arg(lua_State *L, int index, lua_Integer &val,
+                                   lua_Integer def) {
+  val = luaL_optinteger(L, index, def);
+  return val;
+}
+
+static inline lua_State *&opt_arg(lua_State *L, int index, lua_State *&val,
+                                  lua_State *def) {
+  if (lua_isnil(L, index)) {
+    val = def;
+    return val;
+  }
+  luaL_checktype(L, index, LUA_TTHREAD);
+  val = lua_tothread(L, index);
+  return val;
+}
+
+std::optional<TableView> &opt_arg(lua_State *L, int index,
+                                  std::optional<TableView> &val);
+
+static inline void push(lua_State *L, Nil) { lua_pushnil(L); }
+
+static inline void push(lua_State *L, lua_Number val) {
+  lua_pushnumber(L, val);
+}
+
+static inline void push(lua_State *L, bool val) { lua_pushboolean(L, val); }
+
+static inline void push(lua_State *L, std::string_view val) {
+  lua_pushlstring(L, val.data(), val.size());
+}
+
+static inline void push(lua_State *L, const std::string &val) {
+  lua_pushlstring(L, val.data(), val.size());
+}
+
+static inline void push(lua_State *L, const char *val) {
+  lua_pushstring(L, val);
+}
+
+static inline void push(lua_State *L, lua_CFunction val, int nups) {
+  lua_pushcclosure(L, val, nups);
+}
+
+static inline void push(lua_State *L, void *val) {
+  lua_pushlightuserdata(L, val);
+}
+
+static inline void push(lua_State *L, lua_Integer val) {
+  lua_pushinteger(L, val);
+}
 
 } // namespace lege::lua
 
